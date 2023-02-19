c7e0f2e9f24421088fa0282138cdc8d621d6028b
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-02-19 19:52:16 -0300

    feat: add more icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,9 @@
-import hamburguer from './hamburguer.svg'
+import hamburguerWhite from './hamburguer-white.svg'
+import hamburguerGreen from './hamburguer-green.svg'
 import instagram from './instagram-white.svg'
 import facebook from './facebook-white.svg'
 import youtube from './youtube-white.svg'
 import close from './close.svg'
+import whatsappWhite from './whatsapp-white.svg'
 
-export { hamburguer, facebook, instagram, youtube, close }
+export { hamburguerWhite, hamburguerGreen, facebook, instagram, youtube, close, whatsappWhite }
