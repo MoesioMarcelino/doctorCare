115065a1b8c4efb69d3c4331c4333a74970fecae
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-02-19 19:53:56 -0300

    refactor: reorganize styles and project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import { Header } from '@/components'
+import { Banner } from '@/components'
+
+import styles from './styles.module.scss'
 
 export function App() {
-  return <Header />
+  return (
+    <div className={styles.container}>
+      <Banner />
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import './global.scss'
+import { AppProvider } from './contexts'
+import '@/styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 )
