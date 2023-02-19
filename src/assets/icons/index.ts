5915784cd8869e010e96562055ca2b3f98175226
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-02-18 21:11:42 -0300

    feat: import icons to project

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import hamburguer from './hamburguer.svg'
+import instagram from './instagram-white.svg'
+import facebook from './facebook-white.svg'
+import youtube from './youtube-white.svg'
+import close from './close.svg'
+
+export { hamburguer, facebook, instagram, youtube, close }
