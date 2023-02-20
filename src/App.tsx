6d31e9d6e16b6f2985640ada02c69bb2fbc68a98
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-02-20 18:38:47 -0300

    refactor: adjust index html & use contact section & rebuild yarn lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Banner, Services, About } from '@/components'
+import { Banner, Services, About, Contact } from '@/components'
 
 import styles from './styles.module.scss'
 
@@ -8,6 +8,7 @@ export function App() {
       <Banner />
       <Services />
       <About />
+      <Contact />
     </div>
   )
 }
